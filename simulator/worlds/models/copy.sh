b8e1f4a37a131ee3e55df465ba116cf27e13b920
ceballos-alberto <albertoceb98@icloud.com>
2022-01-16 17:27:25 +0100

    Created new Simulation package.

---
+++ simulator/worlds/models/copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------- #
+# ---------------------- Self-Driving Simulator - ARAV ----------------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2021. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
+# Remove saved data from all the folders #
+
+cp -r cube_20k/ ~/.gazebo/models/
+cp -r grey_wall/ ~/.gazebo/models/
+cp -r asphalt_plane/ ~/.gazebo/models/
+cp -r chessActor/ ~/.gazebo/models/
+cp -r chessBoard/ ~/.gazebo/models/
+
+# ---------------------------------------------------------------------------- #
+# ---------------------- Self-Driving Simulator - ARAV ----------------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2021. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
