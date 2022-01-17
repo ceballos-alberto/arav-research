7d411f14f9c0cd181bbf8ed557681486a1d723b3
ceballos-alberto <albertoceb98@icloud.com>
2022-01-17 19:23:17 +0100

    Trying to combine modules.

--- epm/code/AI-Filter.py
+++ epm/code/AI-Filter.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-""" ---------------------------------------------------------------------------- 
+""" ----------------------------------------------------------------------------
     ---------------- Artificial Intelligence Filter (AI-Filter) ----------------
     ----------------------------------------------------------------------------
-    -------------------- Author : Alberto Ceballos Gonzalez -------------------- 
-    -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- 
-    ---------- (c) Copyright 2022 Alberto Ceballos All Rights Reserved ---------  
+    -------------------- Author : Alberto Ceballos Gonzalez --------------------
+    -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr --------
+    ---------- (c) Copyright 2022 Alberto Ceballos All Rights Reserved ---------
     ---------------------------------------------------------------------------- """
 
 # Environment Configuration --> Hide TF Warnings #
@@ -35,28 +35,28 @@ for letter in sys.argv[1]:
 	i+=1
 
 	if letter == "/":
-	
+
 		level+=1;
-	
+
 	if level == 3:
-	
+
 		MODEL_PATH = "../" + sys.argv[1][i:] + "/model"
 		INIT_PATH = "../" + sys.argv[1][i:] + "/data/init.png"
 		SAVE_PATH = "../" + sys.argv[1][i:] + "/output/network"
 		break
-		
-# Topic names #
-		
-INPUT_TOPIC = "/ARAV/EPM/AIFilterInput";
 
-OUTPUT_TOPIC = "/ARAV/EPM/AIFilterOutput";
+# Topic names #
+
+INPUT_TOPIC = "/arav/EPM/AIFilterInput";
+
+OUTPUT_TOPIC = "/arav/EPM/AIFilterOutput";
 
 # Script modes #
 
 if sys.argv[2] == "true":
 
 	display = True
-	
+
 else:
 
 	display = False
@@ -64,7 +64,7 @@ else:
 if sys.argv[3] == "true":
 
 	save = True
-	
+
 else:
 
 	save = False
@@ -72,7 +72,7 @@ else:
 # Constants #
 
 THRESHOLD = 0.5			# This line can be modified #
-FINAL_FRAME = 5			# This line can be modified #
+FINAL_FRAME = 10		# This line can be modified #
 UPDATE_RATE = float (sys.argv[4]) 	# Hz #
 
 # Image Size --> HD 720p #
@@ -86,25 +86,25 @@ class Listener:
 
 	# Constructor method #
 
-	def __init__ (self):		
+	def __init__ (self):
 
 		self.bridge = cv_bridge.CvBridge()
 		self.image = cv2.imread(INIT_PATH)
-		
+
 	# Class methods #
 
 	def imageCallback (self, msg):
-	
+
 		# To be executed when an image is received #
-		
+
 		try:
-		
+
 			self.image = self.bridge.imgmsg_to_cv2 (msg, "bgr8")
 
 		except CvBridgeError as e:
-		
+
 			pass
-			
+
 # Listener declaration #
 
 listener = Listener ()
@@ -124,7 +124,7 @@ sub = rospy.Subscriber(INPUT_TOPIC, Image, listener.imageCallback, queue_size=1)
 pub = rospy.Publisher(OUTPUT_TOPIC, Int16MultiArray, queue_size=1)				# Publisher #
 output_msg = Int16MultiArray ()
 
-rate = rospy.Rate(UPDATE_RATE) 
+rate = rospy.Rate(UPDATE_RATE)
 
 # Load detection model #
 
@@ -134,18 +134,18 @@ print ("[AI-Filter] Inference model has been successfully loaded")
 
 init_time = time.time()
 
-currentFrame = 1 
+currentFrame = 1
 
 # Main loop #
 
 while not rospy.is_shutdown():
 
-	start_time = time.time()		
-	
+	start_time = time.time()
+
 	# ------------------------------------------------------- #
 
 	BoundingBoxes.clear()
-	
+
 	# Load Frame #
 
 	frame = listener.image
@@ -156,8 +156,8 @@ while not rospy.is_shutdown():
 	tensor = tensor[tf.newaxis, ...]
 
 	results = model(tensor)
-	
-	# Transform Output data #							
+
+	# Transform Output data #
 
 	nbCorrectResults = np.argwhere(results['detection_scores'].numpy()[0] > THRESHOLD).shape[0]
 
@@ -167,64 +167,63 @@ while not rospy.is_shutdown():
 		BoundingBoxes.append(int(results['detection_boxes'][0][i][1]*WIDTH))			# Bounding Box X min
 		BoundingBoxes.append(int(results['detection_boxes'][0][i][2]*HEIGHT))		# Bounding Box Y max
 		BoundingBoxes.append(int(results['detection_boxes'][0][i][3]*WIDTH))			# Bounding Box X max
-	
+
 	output_msg.data = BoundingBoxes
-	
+
 	# Publish data #
-	
+
 	pub.publish(output_msg)
-	
+
 	# Stop the node #
-	
+
 	if (currentFrame >= FINAL_FRAME):
 		print ("[AI-Filter] Final Frame Processed >> Killing node")
 		break
 	else:
 		currentFrame += 1
-	
+
 	# Display Output #
-	
+
 	if display:
-	
+
 		for i in range (nbCorrectResults):
-	
+
 			cv2.rectangle (frame, (BoundingBoxes[1+4*i]-5,BoundingBoxes[2+4*i]+5), (BoundingBoxes[3+4*i]+5,BoundingBoxes[0+4*i]-5), (77, 163, 232), 3)
-			
+
 		cv2.imshow("AI-Filter Detection", frame)
-			
+
 		cv2.waitKey(15)
-			
+
 	# Save Output #
-	
+
 	if save:
-	
+
 		if display:
-		
+
 			cv2.imwrite(SAVE_PATH + "/output_" + str(time.time()-init_time)[:4] + "_segs.png", frame)
-			
+
 		else:
-		
+
 			cv2.rectangle (frame, (BoundingBoxes[1+4*i]-5,BoundingBoxes[2+4*i]+5), (BoundingBoxes[3+4*i]+5,BoundingBoxes[0+4*i]-5), (77, 163, 232), 3)
-			
+
 			cv2.imwrite(SAVE_PATH + "/output_" + str(time.time()-init_time)[:4] + "_segs.png", frame)
-			
+
 	# ------------------------------------------------------- #
-	
+
 	end_time = time.time()
-	
+
 	elapsed_time = end_time - start_time
-	
+
 	print ("[AI-Filter] Frame Processed >> Time = {} segs".format(elapsed_time))
-	
+
 	# Wait until next iteration #
-	
+
 	rate.sleep ()
-	
-""" ---------------------------------------------------------------------------- 
+
+""" ----------------------------------------------------------------------------
     ---------------- Artificial Intelligence Filter (AI-Filter) ----------------
     ----------------------------------------------------------------------------
-    -------------------- Author : Alberto Ceballos Gonzalez -------------------- 
-    -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- 
-    ---------- (c) Copyright 2022 Alberto Ceballos All Rights Reserved ---------  
+    -------------------- Author : Alberto Ceballos Gonzalez --------------------
+    -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr --------
+    ---------- (c) Copyright 2022 Alberto Ceballos All Rights Reserved ---------
     ---------------------------------------------------------------------------- """
-    
